31e8227c6eaeac9a64e2eab4327b78614d5b7a72
prashant <prashantchouhan711@gmail.com>
2025-02-21 17:19:58 +0530

    a

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const MobileNav = () => {
   return (
-    <div className='w-full sm:hidden h-[60px] bg-white text-black items-center flex px-5 justify-between'>
-     <h1 className='font-bebas text-2xl'>DEVHACKS</h1>
-     <BarChart className='rotate-90 hover:cursor-pointer'/>
+    <div className='w-full sm:hidden h-[60px] bg-black  items-center flex px-5 justify-between'>
+     <h1 className='font-bebas text-white text-2xl'>DEVHACKS</h1>
+     <BarChart className='rotate-90 hover:cursor-pointer text-white'/>
     </div>
   )
 }
