a1a56f0f49c5587ba319b018aff9e22ea2605c8f
prashant <prashantchouhan711@gmail.com>
2025-02-26 18:14:40 +0530

    sd

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BoxReveal } from './magicui/box-reveal'
 import { Marquee } from './magicui/marquee'
+import Image from 'next/image'
 
 const Sponsors = () => {
   return (
@@ -11,22 +12,32 @@ const Sponsors = () => {
       </h1></BoxReveal>
       <div className='mt-20  gap-10 hidden md:flex flex-col items-center justify-center'>
        <div className='flex  gap-10'>
-       <div className='sponsors'></div>
-       <div className='sponsors'></div> 
-        <div className='sponsors'></div>
+       <div className='sponsors'>
+        <Image height={1000} width={1000} alt='logo' src='/sponsors.png' className=' w-full h-full object-contain '/>
        </div>
-       <div className='flex gap-10'>
-        <div className='sponsors'></div>
-        <div className='sponsors'></div>
+       <div className='sponsors'>
+        <Image height={1000} width={1000} alt='logo' src='/sponsors.png' className=' w-full h-full object-contain '/>
+       </div>
+       <div className='sponsors'>
+        <Image height={1000} width={1000} alt='logo' src='/sponsors.png' className=' w-full h-full object-contain '/>
+       </div>
        </div>
       </div>
       <div className='mt-10 w-full  mb-10 gap-10 flex md:hidden items-center justify-center'>
         <Marquee pauseOnHover>
-          <div className='sponsors'></div>
-          <div className='sponsors'></div>
-          <div className='sponsors'></div>
+        <div className='sponsors'>
+        <Image height={1000} width={1000} alt='logo' src='/sponsors.png' className=' w-full h-full object-contain '/>
+       </div>
+       <div className='sponsors'>
+        <Image height={1000} width={1000} alt='logo' src='/sponsors.png' className=' w-full h-full object-contain '/>
+       </div>
         </Marquee>
       </div>
+      <div className='mt-10 xl:mt-20 items-center justify-center flex flex-col  gap-10 xl:gap-20 w-full mb-20'>
+        <h1 className='font-bebas text-3xl  text-center xl:text-5xl text-white'>in collabration with indore police cyber branch</h1>
+         <div className='sponsors'><Image width={1000} height={1000} alt='police' src='/police.png' className='w-full h-full object-contain bg-white rounded-xl'/></div>
+      </div>
+    
     </div>
 
   )
