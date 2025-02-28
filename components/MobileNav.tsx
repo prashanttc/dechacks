4b9bbfd17aae0a3f81e48b5980bd6ca039b693fa
prashant <prashantchouhan711@gmail.com>
2025-02-28 21:38:09 +0530

    fix unuased dependencies

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { BarChart, X } from "lucide-react";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const MobileNav = () => {
